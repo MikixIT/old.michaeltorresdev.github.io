442458b1fca8e8f9095654a999e170d5dda00ab9
Michael Torres <michael.46@live.it>
2024-03-04 21:28:33 +0100

    new font family and snap scroll working, with new folder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./app.scss";
 function App() {
-  <div>
-    <section>Hero</section>
-    <section>Parallax</section>
-    <section>Projects</section>
-    <section>Contact</section>
-  </div>;
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Projects</section>
+      <section>Contact</section>
+    </div>
+  );
 }
 
 export default App;
