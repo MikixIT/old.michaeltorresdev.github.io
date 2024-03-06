932849ef118b06a95e4d4c3055384d78714b5a59
Michael Torres <michael.46@live.it>
2024-03-06 22:20:58 +0100

    testing framer motion

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Test() {
   return (
     <div className="course">
-      <div className="box"></div>
+      <motion.div
+        className="box"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1, x: 500 }}
+        transition={{ duration: 2, delay: 3 }}
+      ></motion.div>
     </div>
   );
 }
