1d6519ca064a3d87de68a65e3733b1d8ea1b8c0f
Michael Torres <michael.46@live.it>
2024-03-19 20:27:57 +0100

    new component sidebar and delete test comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
-import Test from "./components/Test";
+import Sidebar from "./components/sidebar/Sidebar";
 function App() {
   return (
     <div>
       <section>
         <Navbar />
+        <Sidebar />
       </section>
-      {/* <section>Parallax</section>
-      <section>Projects</section> */}
-      <section>
-        <Test />
-      </section>
-      <section>
-        <Test />
-      </section>
+      <section></section>
+      <section></section>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./sidebar.scss";
+import { motion } from "framer-motion";
+
+function Sidebar() {
+  return <div>Sidebar</div>;
+}
+
+export default Sidebar;
