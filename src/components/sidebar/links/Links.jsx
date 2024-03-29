26c765373bf8de58d00bf08281198c1c28d3cb7d
Michael Torres <michael.46@live.it>
2024-03-29 17:38:34 +0100

    put link at center hamb menu fix

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,8 +1,9 @@
 import "./links.scss";
+import "../sidebar.scss";
 import React from "react";
 
 function Links() {
-  return <div>Links</div>;
+  return <div className="links">Links</div>;
 }
 
 export default Links;
